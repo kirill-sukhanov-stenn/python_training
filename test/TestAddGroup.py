d5a9b6288022bcb0296d36de196872ce0988d954
Kirill Sukhanov <kirill.sukhanov@stenn.com>
2021-12-25 15:51:28 +0300

    Созданы пакеты для разных зон ответственности

--- test/TestAddGroup.py
+++ test/TestAddGroup.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from group import Group
-from application import Application
+from model.group import Group
+from fixture.application import Application
 
 
 @pytest.fixture
 def app(request):
-    fixture=Application()
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
+
 
 def test_add_group(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(name="gdgdj", header="hncn", footer="ghgjhkk"))
     app.logout()
 
+
 def test_add_empty_group(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
     app.logout()
-
