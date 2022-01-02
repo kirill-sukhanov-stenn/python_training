ee62c392f98cd346b94272a7a95045527c98165d
Kirill Sukhanov <kirill.sukhanov@stenn.com>
2022-01-02 20:32:57 +0300

    Повышение интедлектуальности функции login  и logout

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,12 +3,8 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="gdgdj", header="hncn", footer="ghgjhkk"))
-    app.session.logout()
+    app.group.create(Group(name="ytfguyh", header="lkmjmlk", footer="kmlkmkl"))
 
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
