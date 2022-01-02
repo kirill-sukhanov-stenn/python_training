7eb7261755c71c4dc10c4c296675f982bf7b40d1
Kirill Sukhanov <kirill.sukhanov@stenn.com>
2022-01-02 20:33:05 +0300

    Повышение интедлектуальности функции login  и logout

---
+++ fixture/application.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+from fixture.group import GroupHelper
+
+
+class Application:
+    def __init__(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(5)
+        self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/addressbook/")
+
+    def destroy(self):
+        self.wd.quit()
